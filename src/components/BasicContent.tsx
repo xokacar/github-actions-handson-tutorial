5cc153856d84440225b9d033fbb32f4cf7900c92
xokacar <okaykacar31@gmail.com>
2024-02-13 01:43:05 +0300

    feat: update content

--- src/components/BasicContent.tsx
+++ src/components/BasicContent.tsx
@@ -2,12 +2,11 @@ function BasicContent() {
   return (
     <div className="bg-black h-screen flex justify-center items-center">
       <div className="text-center">
-        <h1 className="text-5xl font-bold  text-white">Hands on Github Actions Workflow: CI / CD?</h1>
+        <h1 className="text-5xl font-bold  text-white">Hands on Github Actions Workflow: CI / CD</h1>
         <iframe
           src="https://giphy.com/embed/605EIJNmMuedI87uRf"
           width="480"
           height="457"
-          frameBorder="0"
           allowFullScreen
           title="Funny Cat GIF"
           className="mt-8 mx-auto block"
