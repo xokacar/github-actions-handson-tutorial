8d3d00cb53e871ab2f340f7dab2d1a5838c3af0e
xokacar <okaykacar31@gmail.com>
2024-02-13 17:33:59 +0300

    feat: update heading

--- src/components/BasicContent.tsx
+++ src/components/BasicContent.tsx
@@ -2,7 +2,7 @@ function BasicContent() {
   return (
     <div className="bg-black h-screen flex justify-center items-center">
       <div className="text-center">
-        <h1 className="text-5xl font-bold  text-white">Hands on Github Actions Workflow: CI / CD</h1>
+        <h1 className="text-5xl font-bold  text-white">Hands on Github Actions Workflow : CI / CD</h1>
         <iframe
           src="https://giphy.com/embed/605EIJNmMuedI87uRf"
           width="480"
